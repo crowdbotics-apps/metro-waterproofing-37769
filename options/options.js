fb8a0d55d2b6e559deaadee79f8dca87c1cb12a6
Crowdbotics <team@crowdbotics.com>
2022-12-01 20:22:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "metro_waterproofing_37769",
+  url: "https://metro_waterproofing_37769.botics.co",
+  api: "https://metro_waterproofing_37769.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
